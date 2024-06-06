51e9ac52424602522e883f1fe2a3a8dbc3466922
Kuldeep <pro.kuldeepkumar@gmail.como>
2024-06-06 14:49:53 +0530

    label chage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! Kuldeep</ThemedText>
+        <ThemedText type="title">Welcome! DummyName</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
