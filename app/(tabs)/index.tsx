a42002dbbe3eecb3aeee921b99c3b45e63313c62
dhirajbhatia <dhirajbhatia2004@gmail.com>
2024-06-06 14:45:09 +0530

    label change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! Kuldeep</ThemedText>
+        <ThemedText type="title">Welcome! aman</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
