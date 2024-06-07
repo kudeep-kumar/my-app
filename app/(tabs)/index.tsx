aa81e69824e53745dcea85098a12dc1aa549723f
sharmaaashima <aashimasharma164@gmail.com>
2024-06-07 11:05:08 +0530

    name change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! aashi
+        <ThemedText type="title">Welcome! nav
           +
         </ThemedText>
         <HelloWave />
