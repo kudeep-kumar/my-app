d2c4a25d71f5a45d4b7d8cda5622d7bd2b53dae0
Kuldeep <pro.kuldeepkumar@gmail.como>
2024-06-07 15:39:30 +0530

    component creation

---
+++ app/(tabs)/demo.tsx
@@ -0,0 +1,33 @@
+import { Text, View } from "react-native";
+
+import React from "react";
+
+export default function Demo({ item }) {
+  console.log("here is data", item);
+  return (
+    <View>
+      <Text>Demo</Text>
+    </View>
+  );
+}
+export function DemoOne() {
+  return (
+    <View>
+      <Text>Demo</Text>
+    </View>
+  );
+}
+export function DemoFirst() {
+  return (
+    <View>
+      <Text>Demo</Text>
+    </View>
+  );
+}
+export function DemoSecond() {
+  return (
+    <View>
+      <Text>Demo</Text>
+    </View>
+  );
+}
