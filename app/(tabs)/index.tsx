17bb135111697c9bd57e55f2c9ed6c2b798dcf97
sharmaaashima <aashimasharma164@gmail.com>
2024-06-07 11:03:49 +0530

    label changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! raman</ThemedText>
+        <ThemedText type="title">Welcome! preet</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
