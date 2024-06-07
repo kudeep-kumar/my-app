e4dc7a399a7d6859c4c4d749496d84acb9e3e113
lovedeepkaursonia <lovedeepkaur7306@gmail.com>
2024-06-07 16:15:18 +0530

    name change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! DummyName</ThemedText>
+        <ThemedText type="title">Welcome! love</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
