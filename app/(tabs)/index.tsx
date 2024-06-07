f57c97fd67a47f6b46b67d22cbad20fa9e76f58d
sharmaaashima <aashimasharma164@gmail.com>
2024-06-07 11:20:18 +0530

    change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! nav
+        <ThemedText type="title">welcome! nav
           +
         </ThemedText>
         <HelloWave />
